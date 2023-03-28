048cfbc4b1019f98f5d84f555b3583a5d6e74bf0
Furkan Demir <Furkan Demir@FURKAN>
2023-03-28 21:38:10 +0300

    Add project files.

---
+++ WebAPINorthwind/DesignPatterns/SingletonPattern/DBTool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPINorthwind.Models;
+
+namespace WebAPINorthwind.DesignPatterns.SingletonPattern
+{
+	public class DBTool
+	{
+		DBTool() { }
+
+		static NorthwindEntities _dbInstance;
+
+			public static NorthwindEntities DBInstance
+		{
+			get
+			{
+				if(_dbInstance ==null)_dbInstance = new NorthwindEntities();
+				return _dbInstance;
+			}
+		}
+	}
+}
---
+++ WebAPINorthwind/RequestModel/CategoryCreateReqeustModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPINorthwind.RequestModel
+{
+
+	// Kullanıcı bir Kategori eklemek için nasıl bir veri göndermeli bunu tasarlıyoruız
+	public class CategoryCreateReqeustModel
+	{
+		public string CategoryName { get; set; }
+
+		public string Description { get; set; }
+	}
+}
